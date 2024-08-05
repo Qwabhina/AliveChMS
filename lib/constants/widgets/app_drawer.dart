bab47c5a5cc4adc6f49d314214c822b3b46a82ba
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2024-08-05 16:03:26 +0000

    Update App drawer

--- lib/constants/widgets/app_drawer.dart
+++ lib/constants/widgets/app_drawer.dart
@@ -7,58 +7,107 @@ class AppDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    MyAppState appState = Provider.of<MyAppState>(context);
+    // MyAppState appState = Provider.of<MyAppState>(context);
+    return Consumer<MyAppState>(builder: (context, appState, child) {
     return Container(
-      width: 250,
-      color: Colors.blue,
-      child: Column(
+        width: 300,
+        color: Theme.of(context).colorScheme.background,
+        child: ListView(
         children: [
-          const DrawerHeader(
+            DrawerHeader(
             decoration: BoxDecoration(
-              color: Colors.blue,
+                color: Theme.of(context).colorScheme.background,
             ),
-            child: Text('Menu', style: TextStyle(color: Colors.white)),
+              child: const Text('Menu', style: TextStyle(color: Colors.white)),
           ),
-          ListTile(
-            leading: const Icon(Icons.person, color: Colors.white),
-            title:
-                const Text('Dashboard', style: TextStyle(color: Colors.white)),
-            selected: appState.selectedIndex == 0,
-            selectedTileColor: Colors.blue[700],
-            onTap: () {
-              appState.setCurrentPage(0);
-            },
+            AppDrawerItem(
+              icon: Icons.dashboard,
+              title: 'Dashboard',
+              selected: appState.selectedIndex == 0,
+              onTap: () => appState.setCurrentPage(0, 'Dashboard'),
           ),
-          ListTile(
-            leading: const Icon(Icons.person, color: Colors.white),
-            title: const Text('Profile', style: TextStyle(color: Colors.white)),
-            selected: appState.selectedIndex == 1,
-            selectedTileColor: Colors.blue[700],
-            onTap: () {
-              appState.setCurrentPage(1);
-            },
+            AppDrawerItem(
+              icon: Icons.group_add,
+              title: 'Members & People',
+              selected: appState.selectedIndex == 1,
+              onTap: () => appState.setCurrentPage(1, 'Members & People'),
           ),
-          ListTile(
-            leading: const Icon(Icons.attach_money, color: Colors.white),
-            title: const Text('Finance', style: TextStyle(color: Colors.white)),
-            selected: appState.selectedIndex == 2,
-            selectedTileColor: Colors.blue[700],
-            onTap: () {
-              appState.setCurrentPage(2);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings, color: Colors.white),
-            title:
-                const Text('Settings', style: TextStyle(color: Colors.white)),
-            selected: appState.selectedIndex == 3,
-            selectedTileColor: Colors.blue[700],
-            onTap: () {
-              appState.setCurrentPage(3);
-            },
+            AppDrawerItem(
+              icon: Icons.dashboard,
+              title: 'Groups',
+              selected: appState.selectedIndex == 2,
+              onTap: () => appState.setCurrentPage(2, 'Groups'),
+            ),
+            AppDrawerItem(
+              icon: Icons.dashboard,
+              title: 'Contributions',
+              selected: appState.selectedIndex == 3,
+              onTap: () => appState.setCurrentPage(3, 'Contributions'),
+            ),
+            AppDrawerItem(
+              icon: Icons.dashboard,
+              title: 'Events & Calendar',
+              selected: appState.selectedIndex == 4,
+              onTap: () => appState.setCurrentPage(4, 'Events & Calendar'),
+            ),
+            AppDrawerItem(
+              icon: Icons.dashboard,
+              title: 'Finance',
+              selected: appState.selectedIndex == 5,
+              onTap: () => appState.setCurrentPage(5, 'Finance'),
+            ),
+            AppDrawerItem(
+              icon: Icons.dashboard,
+              title: 'Settings',
+              selected: appState.selectedIndex == 6,
+              onTap: () => appState.setCurrentPage(6, 'Settings'),
           ),
           const Spacer(), // Ensures footer buttons are positioned at the bottom
         ],
+      ),
+    );
+    });
+  }
+}
+
+class AppDrawerItem extends StatelessWidget {
+  const AppDrawerItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.selected,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final String title;
+  final bool selected;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 35),
+      color: Theme.of(context).colorScheme.background,
+      child: ListTile(
+        selectedTileColor: Theme.of(context).colorScheme.secondary,
+        selected: selected,
+        leading: Icon(
+          icon,
+          color: selected
+              ? Theme.of(context).colorScheme.secondary
+              : Theme.of(context).colorScheme.tertiary,
+        ),
+        title: Text(title,
+            style: selected
+                ? TextStyle(
+                    color: Theme.of(context).colorScheme.tertiary,
+                    fontWeight: FontWeight.bold)
+                : TextStyle(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  )),
+        hoverColor: Theme.of(context).colorScheme.secondary,
+        onTap: onTap,
       ),
     );
   }
