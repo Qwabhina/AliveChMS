e5bb97e0e986c2117817fadae272c1483cd7de28
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2024-08-05 13:48:29 +0000

    Create App constants

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,27 @@
+class ImgLinks {
+  static const String appLogo = "assets/aamusted_logo.png";
+  static const String appLogoDark = "assets/aamusted_logo_w.png";
+  static const String appLogoText = "assets/aamusted_flogo_sm.png";
+  static const String appLogoTextDark = "assets/aamusted_flogo_sm_w.png";
+  static const String onboardBeginImg = "assets/onboard_img_fade.png";
+  // static const String onboardBeginImg = "assets/onboard_begin_img.jpg";
+  static const String onboardEndImg = "assets/onboard_img2_fade.png";
+  // static const String onboardEndImg = "assets/onboard_end_img.jpg";
+}
+
+class AppColors {
+  // static final buttonColors = WindowButtonColors(
+  //   iconNormal: primaryBlue,
+  //   mouseOver: primaryBlue,
+  //   mouseDown: const Color.fromARGB(255, 1, 0, 68),
+  //   iconMouseOver: primaryGold,
+  //   iconMouseDown: primaryGold,
+  // );
+
+  // static final closeButtonColors = WindowButtonColors(
+  //   mouseOver: primaryRed,
+  //   mouseDown: const Color.fromARGB(255, 167, 0, 0),
+  //   iconNormal: primaryBlue,
+  //   iconMouseOver: mainLightBg,
+  // );
+}
