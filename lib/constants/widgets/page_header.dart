3713f377c35ce41abcb904aa0abdb6ba55f4e7df
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2024-08-05 13:48:09 +0000

    Create page header widget

---
+++ lib/constants/widgets/page_header.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class PageHeader extends StatelessWidget {
+  final String pageName;
+  const PageHeader({super.key, required this.pageName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              DateFormat.yMMMMEEEEd().format(DateTime.now()),
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.tertiary,
+                fontSize: 14,
+              ),
+            ),
+            Text(
+              pageName,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(height: 10),
+            Container(
+              height: 30,
+              width: 60,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
