cdfed3886648e9ddbc14553e75ced62b3d15e188
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2024-08-08 15:17:42 +0000

    Add offline Data

---
+++ lib/models/data/data_bank.dart
@@ -0,0 +1,406 @@
+Map<String, dynamic> studentData = {
+  "status": "Ok",
+  "firstTime": false,
+  "token":
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lIjoiVGh1IE1heSAxOCAyMDIzIDIxOjE3OjExIEdNVCswMDAwIChHcmVlbndpY2ggTWVhbiBUaW1lKSIsInVzZXJuYW1lIjoiNTE4MTA0MDM0MCIsInN0YXR1cyI6dHJ1ZSwiaWF0IjoxNjg0NDQ0NjMxfQ.SffAy_VZN_OgWj34swsQzpVEN1NekwJ5aMA8ipUMh30",
+  "bio": [
+    {
+      "Title": "Mr.",
+      "Firstname": "Benjamin",
+      "Lastname": "Yankson",
+      "Middlename": "Ebo",
+      "Gender": "Male",
+      "DateOfBirth": "1988-01-25T00:00:00.000Z",
+      "Stream": "fulltime",
+      "Campus": "Kumasi",
+      "StudentType": "Local",
+      "Level": 400,
+      "Telephone": "555000000",
+      "PersonalEmail": "5181040021@st.aamusted.edu.gh",
+      "IndexNumber": "5181040021",
+      "Citizenship": "Ghanaian",
+      "Program": "BSc Cybersecurity",
+      "Department": "Department of Information Technology Education ",
+      "AcademicYear": "2021/2022",
+      "CurrentSemester": "8",
+      "CurrentSemesterName": "Second Semester(400)",
+      "ProgramCode": "cybersec"
+    }
+  ],
+  "grades": [
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2021/2022",
+      "Semester": "Second Semester(400)",
+      "SemesterID": "8",
+      "Grade": "A",
+      "CourseCode": "ITC485",
+      "CreditHours": 3,
+      "CourseName": "Management Information Systems"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2021/2022",
+      "Semester": "Second Semester(400)",
+      "SemesterID": "8",
+      "Grade": "A",
+      "CourseCode": "ICT481",
+      "CreditHours": 3,
+      "CourseName": "Principles of Innovation and Industry"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2021/2022",
+      "Semester": "Second Semester(400)",
+      "SemesterID": "8",
+      "Grade": "A",
+      "CourseCode": "ICT483",
+      "CreditHours": 3,
+      "CourseName": "Distributed Systems"
+    },
+    {
+      "GradePoint": "9.00",
+      "AcadYr": "2021/2022",
+      "Semester": "Second Semester(400)",
+      "SemesterID": "8",
+      "Grade": "B",
+      "CourseCode": "GPD481",
+      "CreditHours": 6,
+      "CourseName": "Post-Intership Seminar"
+    },
+    {
+      "GradePoint": "10.50",
+      "AcadYr": "2021/2022",
+      "Semester": "Second Semester(400)",
+      "SemesterID": "8",
+      "Grade": "B+",
+      "CourseCode": "ITC486",
+      "CreditHours": 3,
+      "CourseName": "Research Project II"
+    },
+    {
+      "GradePoint": "10.50",
+      "AcadYr": "2021/2022",
+      "Semester": "First Semester(400)",
+      "SemesterID": "7",
+      "Grade": "B+",
+      "CourseCode": "GPD471",
+      "CreditHours": 6,
+      "CourseName": "School Internship"
+    },
+    {
+      "GradePoint": "10.50",
+      "AcadYr": "2021/2022",
+      "Semester": "First Semester(400)",
+      "SemesterID": "7",
+      "Grade": "B+",
+      "CourseCode": "GPD472",
+      "CreditHours": 3,
+      "CourseName": "Research Project"
+    },
+    {
+      "GradePoint": "10.50",
+      "AcadYr": "2020/2021",
+      "Semester": "Second Semester(300)",
+      "SemesterID": "6",
+      "Grade": "B+",
+      "CourseCode": "EDC361",
+      "CreditHours": 3,
+      "CourseName": "Pre-Intership Seminar"
+    },
+    {
+      "GradePoint": "7.50",
+      "AcadYr": "2020/2021",
+      "Semester": "Second Semester(300)",
+      "SemesterID": "6",
+      "Grade": "C+",
+      "CourseCode": "EDC362",
+      "CreditHours": 3,
+      "CourseName": "Measurement, Evaluation and Statistics in Education"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2020/2021",
+      "Semester": "Second Semester(300)",
+      "SemesterID": "6",
+      "Grade": "A",
+      "CourseCode": "ITC362",
+      "CreditHours": 3,
+      "CourseName": "Human Computer Interaction"
+    },
+    {
+      "GradePoint": "10.50",
+      "AcadYr": "2020/2021",
+      "Semester": "Second Semester(300)",
+      "SemesterID": "6",
+      "Grade": "A",
+      "CourseCode": "ITC363",
+      "CreditHours": 3,
+      "CourseName": "Network Administration and Security"
+    },
+    {
+      "GradePoint": "9.00",
+      "AcadYr": "2020/2021",
+      "Semester": "Second Semester(300)",
+      "SemesterID": "6",
+      "Grade": "B",
+      "CourseCode": "ITC364",
+      "CreditHours": 3,
+      "CourseName": "Application of IT in Education"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2020/2021",
+      "Semester": "Second Semester(300)",
+      "SemesterID": "6",
+      "Grade": "A",
+      "CourseCode": "ITC366",
+      "CreditHours": 3,
+      "CourseName": "Artificial Intelligence"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2020/2021",
+      "Semester": "First Semester(300)",
+      "SemesterID": "5",
+      "Grade": "A",
+      "CourseCode": "EDC351",
+      "CreditHours": 3,
+      "CourseName": "Guidance and Counselling in Education"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2020/2021",
+      "Semester": "First Semester(300)",
+      "SemesterID": "5",
+      "Grade": "A",
+      "CourseCode": "GPD351",
+      "CreditHours": 3,
+      "CourseName": "Reserach Methods"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2020/2021",
+      "Semester": "First Semester(300)",
+      "SemesterID": "5",
+      "Grade": "A",
+      "CourseCode": "ITC351",
+      "CreditHours": 3,
+      "CourseName": "Web Technologies"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2020/2021",
+      "Semester": "First Semester(300)",
+      "SemesterID": "5",
+      "Grade": "A",
+      "CourseCode": "ITC353",
+      "CreditHours": 3,
+      "CourseName": "Data Communication and Networks"
+    },
+    {
+      "GradePoint": "9.00",
+      "AcadYr": "2020/2021",
+      "Semester": "First Semester(300)",
+      "SemesterID": "5",
+      "Grade": "B",
+      "CourseCode": "ITC356",
+      "CreditHours": 3,
+      "CourseName": "Operating Systems"
+    },
+    {
+      "GradePoint": "12.00",
+      "AcadYr": "2020/2021",
+      "Semester": "First Semester(300)",
+      "SemesterID": "5",
+      "Grade": "A",
+      "CourseCode": "ITC357",
+      "CreditHours": 3,
+      "CourseName": "Software Engineering"
+    },
+  ],
+  "resit_ic": [
+    {
+      "GradePoint": "0.00",
+      "AcadYr": "2021/2022",
+      "Semester": "Second Semester(400)",
+      "Grade": "E",
+      "CourseCode": "ICT 484",
+      "CreditHours": 3,
+      "CourseName": "Digital Media"
+    }
+  ],
+  "transactions": {
+    "100": {
+      "payments": [
+        {
+          "AmtPaid": "1200.00",
+          "Cummulated": "1200.00",
+          "Level": 100,
+          "AcadYear": "2018/2019",
+          "Bank": "Ecobank",
+          "PaymentMode": "cash",
+          "Date": "2023-05-16 13:24:22"
+        },
+        {
+          "AmtPaid": "800.00",
+          "Cummulated": "2000.00",
+          "Level": 100,
+          "AcadYear": "2018/2019",
+          "Bank": "Ecobank",
+          "PaymentMode": "cash",
+          "Date": "2023-05-16 13:25:14"
+        }
+      ],
+      "totalBill": "2000.00",
+      "Currency": "GH¢"
+    },
+    "200": {
+      "payments": [
+        {
+          "AmtPaid": "2000.00",
+          "Cummulated": "2000.00",
+          "Level": 200,
+          "AcadYear": "2019/2020",
+          "Bank": "Ecobank",
+          "PaymentMode": "cash",
+          "Date": "2023-05-16 13:26:01"
+        }
+      ],
+      "totalBill": "2200.00",
+      "Currency": "GH¢"
+    },
+    "300": {
+      "payments": [
+        {
+          "AmtPaid": "2500.00",
+          "Cummulated": "2500.00",
+          "Level": 300,
+          "AcadYear": "2020/2021",
+          "Bank": "Ecobank",
+          "PaymentMode": "cash",
+          "Date": "2022-04-10 13:27:00"
+        },
+        {
+          "AmtPaid": "400.00",
+          "Cummulated": "2900.00",
+          "Level": 300,
+          "AcadYear": "2020/2021",
+          "Bank": "Ecobank",
+          "PaymentMode": "cash",
+          "Date": "2022-08-16 13:27:11"
+        }
+      ],
+      "totalBill": "3000.00",
+      "Currency": "GH¢"
+    },
+    "400": {
+      "payments": [
+        {
+          "AmtPaid": "3800.00",
+          "Cummulated": "3800.00",
+          "Level": 400,
+          "AcadYear": "2021/2022",
+          "Bank": "Ecobank",
+          "PaymentMode": "cash",
+          "Date": "2023-05-16 13:27:47"
+        }
+      ],
+      "totalBill": "3500.00",
+      "Currency": "GH¢"
+    }
+  }
+};
+
+Map<String, dynamic> courseRegistrationData = {
+  "status": "Ok",
+  "toBeRegistered": [
+    {
+      "maxStudent": null,
+      "ID": "custom104",
+      "CourseName": "Cloud Security",
+      "CourseCode": "ICT 481",
+      "CreditHours": 3,
+      "MountedUnder": null,
+      "MountedAs": "core",
+      "children": []
+    },
+    {
+      "maxStudent": null,
+      "ID": "custom105",
+      "CourseName": "Cloud Security",
+      "CourseCode": "ICT 481",
+      "CreditHours": 3,
+      "MountedUnder": null,
+      "MountedAs": "core",
+      "children": []
+    },
+    {
+      "maxStudent": null,
+      "ID": "custom106",
+      "CourseName": "Cloud Security",
+      "CourseCode": "ICT 481",
+      "CreditHours": 3,
+      "MountedUnder": null,
+      "MountedAs": "core",
+      "children": []
+    },
+    {
+      "maxStudent": null,
+      "ID": "custom107",
+      "CourseName": "Cloud Security",
+      "CourseCode": "ICT 481",
+      "CreditHours": 3,
+      "MountedUnder": null,
+      "MountedAs": "core",
+      "children": [
+        {
+          "maxStudent": null,
+          "ID": "custom108",
+          "CourseName": "Cloud Security",
+          "CourseCode": "ICT 481",
+          "CreditHours": 3,
+          "MountedUnder": null,
+          "MountedAs": "core",
+          "children": []
+        }
+      ]
+    },
+    {
+      "maxStudent": null,
+      "ID": "db8640e9e139fe92",
+      "CourseName": "Workplace Experience Learning (NON-SCORING)",
+      "CourseCode": "CSF 475",
+      "CreditHours": 3,
+      "MountedUnder": null,
+      "children": []
+    }
+  ],
+  "registered": [
+    {
+      "IndexNo": "5181040340",
+      "MountedCourse": "custom107",
+      "MountedCourseParent": "custom107",
+      "Date": "2023-05-13T03:04:46.000Z"
+    },
+    {
+      "IndexNo": "5181040340",
+      "MountedCourse": "custom108",
+      "MountedCourseParent": "custom108",
+      "Date": "2023-05-13T03:04:46.000Z"
+    },
+    {
+      "IndexNo": "5181040340",
+      "MountedCourse": "custom104",
+      "MountedCourseParent": "custom104",
+      "Date": "2023-05-13T03:14:03.000Z"
+    },
+    {
+      "IndexNo": "5181040340",
+      "MountedCourse": "custom105",
+      "MountedCourseParent": "custom105",
+      "Date": "2023-05-13T03:14:03.000Z"
+    }
+  ],
+  'rules': []
+};
