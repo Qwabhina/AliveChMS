36e9f86a580f0738331762d08d62af72ba8ea637
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2024-08-08 08:23:43 +0000

    Create Custom Tab view

---
+++ lib/constants/widgets/app_tab_view.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class AppTabView extends StatelessWidget {
+  const AppTabView({
+    super.key,
+    required this.tabs,
+    required this.tabWidgets,
+  });
+
+  final List<Tab> tabs;
+  final List<Widget> tabWidgets;
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: tabs.length,
+      child: Column(
+        children: [
+          TabBar(
+            indicatorSize: TabBarIndicatorSize.tab,
+            isScrollable: true,
+            padding: EdgeInsets.zero,
+            tabAlignment: TabAlignment.center,
+            indicator: BoxDecoration(
+                color: Theme.of(context)
+                    .colorScheme
+                    .surfaceVariant
+                    .withOpacity(0.5),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                )),
+            tabs: tabs,
+          ),
+          // const SizedBox(height: 16),
+          SizedBox(
+            // height: double.maxFinite,
+            height: MediaQuery.of(context).size.height - 240,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context)
+                    .colorScheme
+                    .surfaceVariant
+                    .withOpacity(0.5),
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                ),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 22),
+              child: TabBarView(
+                physics: const BouncingScrollPhysics(
+                  decelerationRate: ScrollDecelerationRate.fast,
+                ),
+                children: tabWidgets,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
