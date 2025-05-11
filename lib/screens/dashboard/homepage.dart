4755936db112f4d1e3e3e45f6926d3d7589f7f01
Benjamin Ebo Yankson <ben.macfinn@gmail.com>
2025-05-11 08:59:15 +0000

    Code update with Backend data

--- lib/screens/dashboard/homepage.dart
+++ lib/screens/dashboard/homepage.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:alivechms/constants/widgets/page_section_header.dart';
 import 'package:alivechms/main.dart';
+import 'package:alivechms/models/dashboard/overview_models.dart';
 import 'package:alivechms/screens/dashboard/dashboard_highlights.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -10,10 +13,15 @@ class HomepageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Map<String, dynamic> user = aspBox.get('user');
-    final List<dynamic> memberData = user['recent_registrations'];
-    final List<Map<String, dynamic>> members =
-        memberData.cast<Map<String, dynamic>>();
+    final OverviewModel overview =
+        OverviewModel.fromJson(aspBox.get("user").cast<String, dynamic>());
+    // print(overview.toString());
+    // final Map<String, dynamic> user =
+    //     aspBox.get('user').cast<String, dynamic>();
+    final List<RecentRegistration> memberData = overview.recentRegistrations;
+    // final List<dynamic> memberData = user['recent_registrations'] as List;
+    // final List<Map<String, dynamic>> members =
+    //     memberData.cast<Map<String, dynamic>>();
 
     return Container(
       margin: const EdgeInsets.only(bottom: 20),
@@ -37,18 +45,18 @@ class HomepageScreen extends StatelessWidget {
             
           ),
           child: Column(
-            children: members.map((member) {
+            children: memberData.map((member) {
               return ListTile(
                 leading: CircleAvatar(
-                  child: Text(member['MbrFirstName']![0]),
+                  child: Text(member.mbrFirstName),
                 ),
                 title: Text(
-                  '${member['MbrFirstName']} ${member['MbrFamilyName']}',
+                  '${member.mbrFirstName} ${member.mbrFamilyName}',
                   style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
                 subtitle: Text(
-                    'Registered: ${DateFormat.yMMMMEEEEd().format(DateTime.parse(member['MbrRegistrationDate']))}'),
-                trailing: Text(member['MbrCustomID']!),
+                    'Registered: ${DateFormat.yMMMMEEEEd().format(DateTime.parse(member.mbrRegistrationDate))}'),
+                trailing: Text(member.mbrCustomID),
                 onTap: () {
                   // Handle tap event
                 },
